8a17737176ec9da05845261c599efb21ca5d4723
Matthew Wollenweber <mjw@insomniac.tech>
2023-09-09 20:12:02 -0500

    testing heroku

--- hal/models.py
+++ hal/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 
+class Mail(models.Model):
+    tdstamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+
 class MailResult(models.Model):
     tdstamp = models.DateTimeField(auto_now_add=True, blank=True)
-    #mailid = models.ForeignKey()
+    # mailid =
     pluginid = models.IntegerField()
     alertid = models.IntegerField()
     severity = models.CharField()
+
+
+class MailIOC(models.Model):
+    tdstamp = models.DateTimeField(auto_now_add=True, blank=True)
+    # mailid = models.ForeignKey()
+
